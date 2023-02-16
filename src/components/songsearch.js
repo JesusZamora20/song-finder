c608b64d3028f9ec529585920125ae9f3003b6ba
JesusZamora20 <jbermudezamora87@gmail.com>
2023-02-15 21:59:13 -0500

    APIs deprecated

--- src/components/songsearch.js
+++ src/components/songsearch.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SongDetails from './songDetails';
 import SongForm from './songForm';
 import { Typography } from '@mui/material';
+import { helpHttp } from '../helpers/helpHttp';
 
 
 function SongSearch() {
@@ -10,8 +11,33 @@ function SongSearch() {
     const [bio, setBio] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        if(search === null) return;
+
+        const fetchData = async() => {
+            const {artist, song} = search;
+
+            let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+            let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+            setLoading(true);
+
+            const [artistRes, songRes] = await Promise.all([
+                helpHttp().get(artistUrl), 
+                helpHttp().get(songUrl)]);
+
+            setBio(artistRes);
+            setLyric(songRes);
+
+            console.log(artistRes,songRes);
+
+            setLoading(false);
+        }
+
+        fetchData();
+    }, [search]);
+
     const handleSearch = (data) => {
-        // console.log(data);
         setSearch(data);
     }
     
